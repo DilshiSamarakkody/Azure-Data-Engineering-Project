077641d5a3cd076d0685b700e2260399330a3744
Dilshi Samarakkody <89601536+DilshiSamarakkody@users.noreply.github.com>
2025-07-30 15:14:39 +0530

    Update create gold views.sql

--- create gold views.sql
+++ create gold views.sql
@@ -1,86 +1,94 @@
 /*
-    CREATE VIEW CALENDER
+    CREATE VIEW DATES
 */
-CREATE VIEW gold.calender
+CREATE VIEW gold.dim_dates
 AS
-SELECT * 
+SELECT 
+ROW_NUMBER() OVER(ORDER BY Date) AS Datekey,
+Date,
+Month,
+Year
 FROM
     OPENROWSET
              (
-                BULK 'https://adventureworksstorageacc.blob.core.windows.net/silver/AdventureWorks_Calender/',
+                BULK 'https://adventureworksstorageacc.blob.core.windows.net/silver/AdventureWorks_Dates/',
                 FORMAT = 'PARQUET'
-              ) AS QUERY01
+              ) AS DATES
 
+GO 
 /*
     CREATE VIEW CUSTOMERS
 */
-CREATE VIEW gold.customers
+CREATE VIEW gold.dim_customers
 AS
-SELECT * 
+SELECT
+ROW_NUMBER() OVER (ORDER BY CustomerKey) AS CustomerKey,
+CustomerKey AS CustomerID,
+Prefix,
+FirstName,
+LastName,
+BirthDate,
+MaritalStatus,
+Gender,
+EmailAddress,
+AnnualIncome,
+TotalChildren,
+EducationLevel,
+Occupation,
+HomeOwner,
+FullName,
+Age
 FROM
     OPENROWSET
              (
                 BULK 'https://adventureworksstorageacc.blob.core.windows.net/silver/AdventureWorks_Customers/',
                 FORMAT = 'PARQUET'
-              ) AS QUERY01
+              ) AS CUSTOMERS
 
-/*
-    CREATE VIEW PRODUCT CATEGORIES
-*/
-CREATE VIEW gold.product_categories
-AS
-SELECT * 
-FROM
-    OPENROWSET
-             (
-                BULK 'https://adventureworksstorageacc.blob.core.windows.net/silver/AdventureWorks_Product_Categories/',
-                FORMAT = 'PARQUET'
-              ) AS QUERY01
-
-/*
-    CREATE VIEW PRODUCT SUBCATEGORIES
-*/
-CREATE VIEW gold.product_sub_categories
-AS
-SELECT * 
-FROM
-    OPENROWSET
-             (
-                BULK 'https://adventureworksstorageacc.blob.core.windows.net/silver/AdventureWorks_Product_Subcategories/',
-                FORMAT = 'PARQUET'
-              ) AS QUERY01
-
+GO
 /*
     CREATE VIEW PRODUCTS
 */
-CREATE VIEW gold.products
+CREATE VIEW gold.dim_products
 AS
-SELECT * 
+SELECT 
+ROW_NUMBER() OVER(ORDER BY productID) AS ProductKey,
+p.productID,
+p.productSku,
+p.productName,
+p.ModelName,
+p.ProductDescription,
+cat.CategoryName AS ProductCategory,
+sub.SubcategoryName AS ProductSubCategory,
+p.ProductColor,
+p.ProductStyle,
+p.ProductCost,
+p.ProductPrice
 FROM
     OPENROWSET
              (
                 BULK 'https://adventureworksstorageacc.blob.core.windows.net/silver/AdventureWorks_Products/',
                 FORMAT = 'PARQUET'
-              ) AS QUERY01
-
-/*
-    CREATE VIEW RETURNS
-*/
-CREATE VIEW gold.returns
-AS
-SELECT * 
-FROM
+              ) AS p
+LEFT JOIN 
     OPENROWSET
              (
-                BULK 'https://adventureworksstorageacc.blob.core.windows.net/silver/AdventureWorks_Returns/',
+                BULK 'https://adventureworksstorageacc.blob.core.windows.net/silver/AdventureWorks_Product_Subcategories/',
                 FORMAT = 'PARQUET'
-              ) AS QUERY01
-
-
+              ) AS sub
+    ON p.ProductSubcategoryKey = sub.ProductSubcategoryKey 
+LEFT JOIN 
+    OPENROWSET
+             (
+                BULK 'https://adventureworksstorageacc.blob.core.windows.net/silver/AdventureWorks_Product_Categories/',
+                FORMAT = 'PARQUET'
+              ) AS cat
+    ON sub.ProductCategoryKey = cat.ProductcategoryKey
+GO
 /*
     CREATE VIEW TERRITORIES
 */
-CREATE VIEW gold.territories
+CREATE VIEW gold.dim_territories
 AS
 SELECT * 
 FROM
@@ -88,31 +96,46 @@ FROM
              (
                 BULK 'https://adventureworksstorageacc.blob.core.windows.net/silver/AdventureWorks_Territories/',
                 FORMAT = 'PARQUET'
-              ) AS QUERY01
+              ) AS ter
+
+GO
 
 /*
-    CREATE VIEW SALES
+    CREATE VIEW FACT_SALES
 */
-CREATE VIEW gold.sales
+CREATE VIEW gold.fact_sales
 AS
-SELECT * 
+SELECT 
+s.OrderNumber,
+s.OrderDate,
+s.OrderLineItem,
+s.OrderQuantity,
+s.StockDate,
+p.ProductKey,
+c.CustomerKey,
+t.TerritoryKey,
+d.Datekey AS OrderDateKey,
+r.ReturnDate,
+r.ReturnQuantity
 FROM
     OPENROWSET
              (
-                BULK 'https://adventureworksstorageacc.blob.core.windows.net/silver/AdventureWorks_sales/',
+                BULK 'https://adventureworksstorageacc.blob.core.windows.net/silver/AdventureWorks_Sales/',
                 FORMAT = 'PARQUET'
-              ) AS QUERY01
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+              ) AS s
+LEFT JOIN
+    OPENROWSET
+             (
+                BULK 'https://adventureworksstorageacc.blob.core.windows.net/silver/AdventureWorks_Returns/',
+                FORMAT = 'PARQUET'
+              ) AS r
+    ON s.Productkey = r.ProductKey
+    AND s.TerritoryKey = r.TerritoryKey
+LEFT JOIN gold.dim_customers c
+    ON s.CustomerKey = c.CustomerID
+LEFT JOIN gold.dim_products p
+    ON s.ProductKey = p.productID
+LEFT JOIN gold.dim_territories t
+    ON s.TerritoryKey = t.TerritoryKey
+LEFT JOIN gold.dim_dates d
+    ON s.OrderDate = d.Date
